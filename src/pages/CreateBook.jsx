43558288b7b870563f8d2b2bd3d5b05e1cd5eac8
Prasenjit Das <132381731+prasenjitpriyan@users.noreply.github.com>
2023-11-20 19:28:27 +0530

    Update CreateBook.jsx

--- src/pages/CreateBook.jsx
+++ src/pages/CreateBook.jsx
@@ -21,7 +21,7 @@ const CreateBook = () => {
         };
         setLoading(true);
         axios
-            .post("http://localhost:3001/books", data)
+            .post("https://booklistback.onrender.com/books", data)
             .then(() => {
                 setLoading(false);
                 enqueueSnackbar('Book Created Successfully', { variant: 'success' })
