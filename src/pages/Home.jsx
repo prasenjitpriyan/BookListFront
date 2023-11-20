193fec1756c51105ed4df4b63968acff0d44c660
Prasenjit Das <132381731+prasenjitpriyan@users.noreply.github.com>
2023-11-20 19:29:51 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
 
     useEffect(() => {
         setLoading(true);
-        axios.get("http://localhost:3001/books")
+        axios.get("https://booklistback.onrender.com/books")
             .then((response) => {
                 setBooks(response.data.data);
                 setLoading(false)
